b9ffb02c046fd144fff3997f5f5a1cbf240b7e00
白衣卿相 <dingruofan1@foxmail.com>
2022-01-19 09:24:59 +0800

    honor commit

--- R/GTExanalyze.R
+++ R/GTExanalyze.R
@@ -18,30 +18,33 @@
 #'    utils::download.file(gwasURL, destfile=gwasFile)
 #'    gwasDF <- data.table::fread(gwasFile, sep="\t")
 #'    gwasDF <- gwasDF[, .(rsid, chr, position, P, maf)]
-#'    traits <- GTExanalyze_traitDetect(gwasDF)
+#'    sentinelSnps <- GTExanalyze_getSentinelSnp(gwasDF)
+#'
+#'    gwasDF <- fread("D:\\R_project\\GLGC_CG0052_result.txt.gz", sep="\t")
+#'    gwasDF <- gwasDF[,.(rsid, chr, position, `p-value`, maf)]
+#'    sentinelSnps <- GTExanalyze_getSentinelSnp(gwasDF)
 #' }
-GTExanalyze_traitDetect <- function(gwasDF, pValueThreshold=5e-8, colocRange=1e6, tissueSiteDetail="Brain - Cortex", maf_threshold=0.01 ){
+GTExanalyze_getSentinelSnp <- function(gwasDF, pValueThreshold=5e-8, centerRange=1e6 ){
   # Detect gene with sentinal SNP:
   gwasDF <- gwasDF[,1:5]
-  names(gwasDF) <- c("rsid", "chr", "position", "P", "maf")
+  names(gwasDF) <- c("rsid", "chr", "position", "pValue", "maf")
   gwasDF <- na.omit(gwasDF)
   # convert variable class:
-  gwasDF[,c("position", "P", "maf")] <- gwasDF[,.(position=as.numeric(position), P=as.numeric(P), maf=as.numeric(maf))]
+  gwasDF[,c("position", "pValue", "maf")] <- gwasDF[,.(position=as.numeric(position), pValue=as.numeric(pValue), maf=as.numeric(maf))]
 
   # MAF filter:
-  gwasDF <- gwasDF[maf>maf_threshold & maf<1,]
+  # gwasDF <- gwasDF[maf>maf_threshold & maf<1,]
   # 去重：
-  gwasDF <- gwasDF[order(rsid, P)][!duplicated(rsid)]
+  # gwasDF <- gwasDF[order(rsid, pValue)][!duplicated(rsid)]
   # retain SNPs with rs id:
-  gwasDF <- gwasDF[stringr::str_detect(rsid,stringr::regex("^rs")),]
+  # gwasDF <- gwasDF[stringr::str_detect(rsid,stringr::regex("^rs")),]
 
   # sentinel snp:
-  gwasDFsub <- gwasDF[P<pValueThreshold, ][order(P, position)]
-
+  gwasDFsub <- gwasDF[pValue<pValueThreshold, ][order(pValue, position)]
 
   chrAll <- unique(gwasDFsub$chr)
   sentinelSnps <- data.table()
-  message("Start the detection of sentinel SNPs: ")
+  message("== Start the detection of sentinel SNPs: ")
   for(i in 1:length(chrAll)){
     gwasDFsubChrom <- gwasDFsub[chr==chrAll[i],]
     tmp <- copy(gwasDFsubChrom)
@@ -49,56 +52,88 @@ GTExanalyze_traitDetect <- function(gwasDF, pValueThreshold=5e-8, colocRange=1e6
     while(nrow(tmp)>0){
       sentinelSnps_count <- sentinelSnps_count+1
       sentinelSnps <- rbind(sentinelSnps, tmp[1,])
-      startPos <- tmp[1,]$position-colocRange
-      endPos <- tmp[1,]$position+colocRange
-      tmp <- tmp[position<startPos | position>endPos][order(P, position)]
+      startPos <- tmp[1,]$position-centerRange/2
+      endPos <- tmp[1,]$position+centerRange/2
+      tmp <- tmp[position<startPos | position>endPos][order(pValue, position)]
     }
-    message("In ",chrAll[i], ", ",sentinelSnps_count, " sentinel SNPs detected. ")
+    message("   In ",chrAll[i], ", ",sentinelSnps_count, " sentinel SNPs detected. ")
     rm(tmp, sentinelSnps_count, startPos, endPos)
   }
-
-
-
-  # 1. GTEx significant eQTL:
-  # gwasSNPeqtl <- rbindlist(lapply(1:nrow(gwasDFsub), function(i){
-  #   message(i,"/",nrow(gwasDFsub)," | " ,gwasDFsub$rsid[i])
-  #   tmp1 <- suppressMessages( GTExdownload_eqtlSig(variantName= gwasDFsub$rsid[i],  datasetId="gtex_v8", tissueSiteDetail="Brain - Cortex") )
-  #   if( !is.null(tmp1) && nrow(tmp1)!=0 ){
-  #     return(tmp1)
-  #   }else{
-  #     return(NULL)
-  #   }
-  # }))
-
-  #2. Gene range:
-  genes_txdb <- suppressMessages( GenomicFeatures::genes(TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene) )
-  genes_txdb <- cbind(data.table(chr=as.character(GenomeInfoDb::seqnames(genes_txdb))), as.data.table(IRanges::ranges(genes_txdb)))
-  genes_txdb <- genes_txdb[chr %in% paste0("chr",c(1:22,"X","Y"))]
-  genes_txdb$start <-genes_txdb$start - upDownStream
-  genes_txdb$end <-genes_txdb$end + upDownStream
-  # merge:
-  targetGene <- data.table()
-  for( c in unique(gwasDFsub$chr)){
-    geneTmp <- genes_txdb[chr==c]
-    gwasDFsubTmp <- gwasDFsub[chr==c]
-    a <- data.table::rbindlist(lapply(1:nrow(gwasDFsubTmp), function(x){ pos1=as.integer(gwasDFsubTmp[x]$position); hited <- geneTmp[pos1 - start >=0 & pos1-end<=0 ]; if(nrow(hited)==0){return(NULL)}else{ hited <- cbind(hited, gwasDFsubTmp[x,c("rsid", "position")]);hited$id=x;return(hited) } }))
-    targetGene <- rbind(targetGene, a)
-    message( "chrom: ", c)
-  }
-  if(nrow(targetGene)==0){
-    message("Please reset pValueThreshold a greater value.")
-    return(NULL)
-  }
-  targetGeneUniq <- targetGene[,.( rsids=paste0(rsid, collapse = ", ")),by = c("chr", "start", "end", "width", "names")]
-  # gene annotation:
-  targetGeneAnno <- AnnotationDbi::select(org.Hs.eg.db, keys=targetGeneUniq$names, columns=c("SYMBOL", "ENSEMBL"), keytype="ENTREZID")
-  targetGeneUniqAnno <- merge( targetGeneAnno, targetGeneUniq, by.x="ENTREZID", by.y="names")
-  return(targetGeneUniqAnno)
-
-  # 3. ldlink
-  # tissueSiteDetailId <- tissueSiteDetailGTExv8[tissueSiteDetail, on="tissueSiteDetail"]$tissueSiteDetailId
-  # a <- LDexpress(gwasDFsub$rsid[1:10],r2d_threshold=1, tissue = tissueSiteDetailId, pop = population, token = "9246d2db7917" )
+  message("== Totally, ", nrow(sentinelSnps), " sentinel SNPs located in ",length(unique(sentinelSnps$chr))  ," chromosomes have been detected!")
+  return(sentinelSnps)
 }
+
+GTExanalyze_getTraits <- function(sentinelSnpDF, colocRange=1e6, genome="grch38"){
+  # tissueSiteDetail="Brain - Cortex", maf_threshold=0.01
+  if(genome == "grch38"){
+    geneInfo <- extractGeneInfo(gencodeGeneInfoAllGranges, genomeVersion = "V26")
+  }else{
+    geneInfo <- extractGeneInfo(gencodeGeneInfoAllGranges, genomeVersion = "V19")
+  }
+
+  message("== Start : ")
+  chrAll <- unique(sentinelSnpDF$chr)
+  traitsAll <- data.table()
+  for(i in 1:length(chrAll)){
+    sentinelSnpChrom <- sentinelSnpDF[chr==chrAll[i],]
+    geneInfoChrom <- geneInfo[chromosome == chrAll[i]]
+
+    Traits <- rbindlist(lapply(1:nrow(sentinelSnpChrom), function(x){
+      tmp<-sentinelSnpChrom[x,];
+      startPos <- tmp$position - colocRange/2; endPos <- tmp$position + colocRange/2;
+      traits <- geneInfoChrom[ (startPos>=start & startPos<=end) | (endPos>=start & endPos<=end) ];
+      # traits$rsid <- tmp$rsid; traits$position <- tmp$position;traits$position <- tmp$position;
+      traits[,c("rsid","position","pValue","maf"):=.(tmp$rsid, tmp$position, tmp$pValue, tmp$maf)];
+      return(traits)
+    }))
+    Traits$chromosome <- chrAll[i]
+    traitsAll <- rbind(traitsAll, Traits)
+    message(i," - ", chrAll[i]," - ",nrow(Traits))
+    rm(sentinelSnpChrom, geneInfoChrom, Traits)
+  }
+  message("== End : ")
+}
+
+
+# 1. GTEx significant eQTL:
+# gwasSNPeqtl <- rbindlist(lapply(1:nrow(gwasDFsub), function(i){
+#   message(i,"/",nrow(gwasDFsub)," | " ,gwasDFsub$rsid[i])
+#   tmp1 <- suppressMessages( GTExdownload_eqtlSig(variantName= gwasDFsub$rsid[i],  datasetId="gtex_v8", tissueSiteDetail="Brain - Cortex") )
+#   if( !is.null(tmp1) && nrow(tmp1)!=0 ){
+#     return(tmp1)
+#   }else{
+#     return(NULL)
+#   }
+# }))
+
+# #2. Gene range:
+# genes_txdb <- suppressMessages( GenomicFeatures::genes(TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene) )
+# genes_txdb <- cbind(data.table(chr=as.character(GenomeInfoDb::seqnames(genes_txdb))), as.data.table(IRanges::ranges(genes_txdb)))
+# genes_txdb <- genes_txdb[chr %in% paste0("chr",c(1:22,"X","Y"))]
+# genes_txdb$start <-genes_txdb$start - upDownStream
+# genes_txdb$end <-genes_txdb$end + upDownStream
+# # merge:
+# targetGene <- data.table()
+# for( c in unique(gwasDFsub$chr)){
+#   geneTmp <- genes_txdb[chr==c]
+#   gwasDFsubTmp <- gwasDFsub[chr==c]
+#   a <- data.table::rbindlist(lapply(1:nrow(gwasDFsubTmp), function(x){ pos1=as.integer(gwasDFsubTmp[x]$position); hited <- geneTmp[pos1 - start >=0 & pos1-end<=0 ]; if(nrow(hited)==0){return(NULL)}else{ hited <- cbind(hited, gwasDFsubTmp[x,c("rsid", "position")]);hited$id=x;return(hited) } }))
+#   targetGene <- rbind(targetGene, a)
+#   message( "chrom: ", c)
+# }
+# if(nrow(targetGene)==0){
+#   message("Please reset pValueThreshold a greater value.")
+#   return(NULL)
+# }
+# targetGeneUniq <- targetGene[,.( rsids=paste0(rsid, collapse = ", ")),by = c("chr", "start", "end", "width", "names")]
+# # gene annotation:
+# targetGeneAnno <- AnnotationDbi::select(org.Hs.eg.db, keys=targetGeneUniq$names, columns=c("SYMBOL", "ENSEMBL"), keytype="ENTREZID")
+# targetGeneUniqAnno <- merge( targetGeneAnno, targetGeneUniq, by.x="ENTREZID", by.y="names")
+# return(targetGeneUniqAnno)
+
+# 3. ldlink
+# tissueSiteDetailId <- tissueSiteDetailGTExv8[tissueSiteDetail, on="tissueSiteDetail"]$tissueSiteDetailId
+# a <- LDexpress(gwasDFsub$rsid[1:10],r2d_threshold=1, tissue = tissueSiteDetailId, pop = population, token = "9246d2db7917" )
 
 #' @title coloc analysis with deteched trait
 #'
