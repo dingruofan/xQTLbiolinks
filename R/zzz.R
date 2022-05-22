6499913909abc4d639bdf05478085d6fdd5b08cf
白衣卿相 <dingruofan1@foxmail.com>
2022-05-23 00:56:17 +0800

    1

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+##' @importFrom utils packageDescription
+.onAttach <- function(libname, pkgname) {
+  pkgVersion <- packageDescription(pkgname, fields="Version")
+  msg <- paste0(pkgname, " v", pkgVersion, "  ",
+                "For help: https://github.com/lilab-bioinfo/xQTLbiolinks/", "\n\n")
+
+  # if (capabilities("libcurl")) {
+  #   dl.method <- "libcurl"
+  # } else {
+  #   dl.method <- getOption("download.file.method", default = "auto")
+  # }
+
+  # options(clusterProfiler.download.method = dl.method)
+  # options(timeout = max(300, getOption("timeout"))) # see ?download.file
+
+  citation <- paste0("If you use ", pkgname, " in published research, please cite:\n",
+                     "Ruofan Ding, Xudong Zou, Gao Wang, Lei Li. xQTLbiolinks: an R/Bioconductor package for integrative analysis of xQTL data.",
+                     "")
+
+
+  packageStartupMessage(paste0(msg, citation))
+}
