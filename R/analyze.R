85e21405296d2f66ac51206e8fb2385cd42e4a43
白衣卿相 <dingruofan1@foxmail.com>
2022-10-08 21:27:44 +0800

    xQTLanalyze_anno

--- R/analyze.R
+++ R/analyze.R
@@ -936,10 +936,360 @@ xQTLanalyze_propensity <- function(gene="", geneType="auto", variantName="", var
 
 
 
+#
+# qtl1 <- xQTLdownload_eqtlSig(genes="MLH1")
+# qtl1 <- cbind(qtl1, rbindlist(lapply(qtl1$variantId, function(x){ a=stringr::str_split(x, "_")[[1]];data.table(chrom=a[1], pos=as.numeric(a[2]), ref=a[3], alt=a[4]) })) )
+# snpInfo <- unique(qtl1[,.(chrom, pos, ref, alt)])
+#
+# qtl <- xQTLdownload_eqtlAllAsso(gene="MMP7", tissueLabel = "Prostate")
+# qtl$chrom <- ifelse(stringr::str_detect(qtl$chrom, stringr::regex("^chr")), qtl$chrom, paste0("chr",qtl$chrom))
+# qtl <- na.omit(qtl)
+# snpInfo <- unique(qtl[,.(chrom, pos, ref, alt)])
+#
+# varAlleles <- Biostrings::DNAStringSet(snpInfo$alt)
+#
 
 
+#' @title annotate variants with genome positon
+#'
+#' @param snpInfo A data.table/data.frame with two or three columns: chromosome and position.
+#' @param genomeVersion "hg38" (default) or "hg19". Note: hg19 will be converted to hg38 automatically.
+#' @import BSgenome.Hsapiens.UCSC.hg38
+#' @import data.table
+#' @import stringr
+#' @importFrom GenomicRanges GRanges
+#' @importFrom GenomicFeatures intronicParts exonParts
+#' @importFrom IRanges findOverlaps
+#' @importFrom VariantAnnotation locateVariants CodingVariants
+#'
+#' @return A data.table object of variants' genomics distribution
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' url1 <- "http://github.com/dingruofan/exampleData/raw/master/gwas/gwasSub.txt.gz"
+#' snpInfo <- fread(url1, sep="\t")
+#' }
+# xQTLanalyze_anno <- function(snpInfo="", genomeVersion="hg38", pValueBy=5){
+#
+#   message("==> Start checking variants...")
+#   # check snpinfo:
+#   snpInfo <- data.table::as.data.table(snpInfo[,1:3])
+#   snpInfo <- na.omit(snpInfo)
+#   names(snpInfo) <- c("chrom", "pos", "pValue")
+#   snpInfo$chrom <- as.character(snpInfo$chrom)
+#   snpInfo$pos <- as.integer(snpInfo$pos)
+#   snpInfo$pValue <- as.numeric(snpInfo$pValue)
+#   snpInfo <- snpInfo[chrom %in% paste0("chr",c(1:22,"X","Y","M"))]
+#   snpInfo <- snpInfo[pValue>0,]
+#   if(nrow(snpInfo)<1){
+#     stop("Number of variants < 1...")
+#   }
+#   # create snpinfo Grange object:
+#   snpRanges <- GenomicRanges::GRanges(snpInfo$chrom,
+#                                       IRanges::IRanges(snpInfo$pos, snpInfo$pos),
+#                                       strand= "*",
+#                                       snpInfo[,"pValue"]
+#   )
+#   snpInfoNew <- data.table::data.table()
+#   snpRanges_hg38 <- GenomicRanges::GRanges()
+#   if(genomeVersion == "hg19"){
+#     message("== Converting vairants' coordinate to hg38... ")
+#
+#     if(suppressMessages(!requireNamespace("rtracklayer"))){
+#       message("Package [rtracklayer] is not installed! please install [rtracklayer] with following: ")
+#       message("---------")
+#       message('\"if (!require("BiocManager", quietly = TRUE)); BiocManager::install("rtracklayer")\"')
+#       message("---------")
+#     }
+#
+#     path = system.file(package="xQTLbiolinks", "extdata", "hg19ToHg38.over.chain")
+#     ch = rtracklayer::import.chain(path)
+#     snpRanges_hg38 <- unlist(rtracklayer::liftOver(snpRanges, ch))
+#     # retain the SNPs located in chromosome 1:22
+#     snpRanges_hg38 <- snpRanges_hg38[which(as.character(GenomeInfoDb::seqnames(snpRanges_hg38)) %in% paste0("chr", c(1:22,"X","Y", "M"))),]
+#     message("== ",length(snpRanges_hg38),"/",nrow(snpInfo)," (",round(length(snpRanges_hg38)/nrow(snpInfo)*100,2),"%)"," left.")
+#     snpInfoNew <- data.table::data.table(chrom = as.character(GenomeInfoDb::seqnames(snpRanges_hg38)), pos=as.data.table(IRanges::ranges(snpRanges_hg38))$start, pValue=snpRanges_hg38$pValue)
+#     snpRanges <- data.table::copy(snpRanges_hg38)
+#     snpInfo <- data.table::copy(snpInfoNew)
+#     rm(path, ch, snpInfoNew, snpRanges_hg38)
+#   }
+#   txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene
+#
+#   ############## import datasets:
+#   message("==> Start import annotations...")
+#   message("      cpg...")
+#   cpgAnno <- fread(system.file(package = "xQTLbiolinks", "extdata", "cpgLsland.bed.gz"), header=FALSE)
+#   cpgRanges <- GenomicRanges::GRanges(cpgAnno$V1, IRanges::IRanges(cpgAnno$V2, cpgAnno$V3), strand= "*")
+#
+#   # import enhancer dataset:
+#   message("      enhancer...")
+#   enhancerAnno <- fread(system.file(package = "xQTLbiolinks", "extdata", "geneHancer.bed.gz"), header=FALSE)
+#   enhancerRanges <- GenomicRanges::GRanges(enhancerAnno$V1, IRanges::IRanges(enhancerAnno$V2, enhancerAnno$V3), strand= "*")
+#
+#   # import tf cluster dataset:
+#   message("      tf cluster...")
+#   tfAnno <- fread(system.file(package = "xQTLbiolinks", "extdata", "tf_filtered_sorted_merge_sub.bed.gz"), header=FALSE)
+#   tfRanges <- GenomicRanges::GRanges(tfAnno$V1,IRanges::IRanges(tfAnno$V2, tfAnno$V3), strand= "*")
+#
+#   # extract introns:
+#   message("      introns...")
+#   intronParts <-  GenomicFeatures::intronicParts(txdb)
+#   intronParts <- intronParts[which(as.character(GenomeInfoDb::seqnames(intronParts)) %in% paste0("chr", c(1:22,"X","Y", "M"))),]
+#   intronAnno <- as.data.table(intronParts)
+#
+#   # extract exons:
+#   message("      exons...")
+#   exonParts <- GenomicFeatures::exonicParts(txdb)
+#   exonParts <- exonParts[which(as.character(GenomeInfoDb::seqnames(exonParts)) %in% paste0("chr", c(1:22,"X","Y", "M"))),]
+#   exonAnno <- as.data.table(exonParts)
+#
+#   # extract promoters:
+#   message("      promoters...")
+#   promoterParts <- suppressWarnings(GenomicFeatures::promoters(txdb))
+#   promoterParts <- promoterParts[which(as.character(GenomeInfoDb::seqnames(promoterParts)) %in% paste0("chr", c(1:22,"X","Y", "M"))),]
+#
+#   # all transcripts:
+#   txinfo <- as.data.table(transcripts(txdb))
+#   txinfo <- txinfo[which(seqnames %in% paste0("chr", c(1:22,"X","Y", "M"))),]
+#
+#   ############ start find overlap with annotations:
+#   message("==> Start annotate variants...")
+#   cpgHits <- as.data.table(SummarizedExperiment::findOverlaps(query = snpRanges, subject = cpgRanges, maxgap = 1, ignore.strand=TRUE ))
+#   cpgHits <- cbind(snpInfo[cpgHits$queryHits,],cpgAnno[cpgHits$subjectHits,.(anno=paste0(V1,":",V2,"-",V3), type="cpg")])
+#   message("      cpg hits: ",nrow(cpgHits))
+#   rm(cpgAnno, cpgRanges)
+#
+#   # annotate snp with enhancers:
+#   enhancerHits <- as.data.table(SummarizedExperiment::findOverlaps(query = snpRanges, subject = enhancerRanges, maxgap = 1, ignore.strand=TRUE ))
+#   enhancerHits <- cbind(snpInfo[enhancerHits$queryHits,],enhancerAnno[enhancerHits$subjectHits,.(anno=paste0(V1,":",V2,"-",V3), type="enhancer")])
+#   message("      enhancer hits: ",nrow(enhancerHits))
+#   rm(enhancerAnno, enhancerRanges)
+#
+#   # annotate snp with tf clusters:
+#   tfHits <- as.data.table(SummarizedExperiment::findOverlaps(query = snpRanges, subject = tfRanges, maxgap = 1, ignore.strand=TRUE ))
+#   tfHits <- cbind(snpInfo[tfHits$queryHits,],tfAnno[tfHits$subjectHits,.(anno=paste0(V1,":",V2,"-",V3), type="tfCluster")])
+#   message("      tf hits: ",nrow(tfHits))
+#   rm(tfAnno, tfRanges)
+#
+#   # annotate snp with introns:
+#   intronHits <- as.data.table(SummarizedExperiment::findOverlaps(query = snpRanges, subject = intronParts, maxgap = 1, ignore.strand=TRUE ))
+#   if(nrow(intronHits)>0){
+#     intronHits <- cbind(snpInfo[intronHits$queryHits,],intronAnno[intronHits$subjectHits,.(anno=tx_name, type="exon")])
+#     intronHits$anno <- unlist(lapply(intronHits$anno, convertCharacter))
+#     # collapse annotation:
+#     intronHits <- intronHits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   }else{
+#     intronHits <- cpgHits[0,]
+#   }
+#   message("      intron hits: ",nrow(intronHits))
+#   rm(intronParts, intronAnno, convertCharacter)
+#
+#   # annotate snp with exons:
+#   exonHits <- as.data.table(SummarizedExperiment::findOverlaps(query = snpRanges, subject = exonParts, maxgap = 1, ignore.strand=TRUE ))
+#   convertCharacter <- function(x){ a=as.list(x);paste0(unlist(a), collapse = ",") }
+#   if(nrow(exonHits)>0){
+#     exonHits <- cbind(snpInfo[exonHits$queryHits,],exonAnno[exonHits$subjectHits,.(anno=tx_name, type="exon")])
+#     exonHits$anno <- unlist(lapply(exonHits$anno, convertCharacter))
+#     # collapse annotation:
+#     exonHits <- exonHits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   }else{
+#     exonHits <- cpgHits[0,]
+#   }
+#   message("      exon hits: ",nrow(exonHits))
+#   rm(exonParts, exonAnno)
+#
+#   # annotate snp with promoters:
+#   promoterHits <- as.data.table(SummarizedExperiment::findOverlaps(query = snpRanges, subject = promoterParts, maxgap = 1, ignore.strand=TRUE ))
+#   promoterHits <- cbind(snpInfo[promoterHits$queryHits,], as.data.table(mcols(promoterParts))[promoterHits$subjectHits,.(anno=tx_name, type="promoter")])
+#   # collapse annotation:
+#   promoterHits <- promoterHits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   message("      promoter hits: ",nrow(promoterHits))
+#   rm(promoterParts)
+#
+#   # annotate snp with cds:
+#   cdsHits <- suppressWarnings( as.data.table(VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::CodingVariants())) )
+#   if(nrow(cdsHits)>0){
+#     cdsHits <- cbind(snpInfo[cdsHits$QUERYID,], cdsHits[,.(tx_id=as.integer(TXID),type="cds")])
+#     cdsHits <- merge(cdsHits, txinfo[,.(tx_id,anno=tx_name)],by="tx_id")[,-c("tx_id")]
+#     # collapse annotation:
+#     cdsHits <- cdsHits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   }else{
+#     cdsHits <- cpgHits[0,]
+#   }
+#   message("      cds hits: ",nrow(cdsHits))
+#
+#   # annotate snp with 5 utr:
+#   utr5Hits <- suppressWarnings( as.data.table(VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::FiveUTRVariants())) )
+#   if(nrow(utr5Hits)>0){
+#     utr5Hits <- cbind(snpInfo[utr5Hits$QUERYID,], utr5Hits[,.(tx_id=as.integer(TXID),type="utr5")])
+#     utr5Hits <- merge(utr5Hits, txinfo[,.(tx_id,anno=tx_name)],by="tx_id")[,-c("tx_id")]
+#     # collapse annotation:
+#     utr5Hits <- utr5Hits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   }else{
+#     utr5Hits <- cpgHits[0,]
+#   }
+#   message("      5'utr hits: ",nrow(utr5Hits))
+#
+#   # annotate snp with 3 utr:
+#   utr3Hits <- suppressWarnings( as.data.table(VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::ThreeUTRVariants())) )
+#   if(nrow(utr3Hits)>0){
+#     utr3Hits <- cbind(snpInfo[utr3Hits$QUERYID,], utr3Hits[,.(tx_id=as.integer(TXID),type="utr3")])
+#     utr3Hits <- merge(utr3Hits, txinfo[,.(tx_id,anno=tx_name)],by="tx_id")[,-c("tx_id")]
+#     # collapse annotation:
+#     utr3Hits <- utr3Hits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   }else{
+#     utr3Hits <- cpgHits[0,]
+#   }
+#   message("      3'utr hits: ",nrow(utr3Hits))
+#
+#   # annotate snp with splice sites:
+#   splicingHits <- suppressWarnings( as.data.table(VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::SpliceSiteVariants())) )
+#   if(nrow(splicingHits)>0){
+#     splicingHits <- cbind(snpInfo[splicingHits$QUERYID,], splicingHits[,.(tx_id=as.integer(TXID),type="splicsSite")])
+#     splicingHits <- merge(splicingHits, txinfo[,.(tx_id,anno=tx_name)],by="tx_id")[,-c("tx_id")]
+#     # collapse annotation:
+#     splicingHits <- splicingHits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#   }else{
+#     splicingHits <- cpgHits[0,]
+#   }
+#   message("      splice site hits: ",nrow(splicingHits))
+#
+#   intergenicHits <- suppressWarnings( as.data.table(VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::IntergenicVariants(), ignore.strand=TRUE)) )
+#   if(nrow(intergenicHits)>0){
+#     fetchLastElement <- function(x){ a=unlist(as.list(x)); ifelse(length(a)==0,"", a[length(a)]) }
+#     fetchFirstElement <- function(x){ a=unlist(as.list(x));  ifelse(length(a)==0,"", a[1]) }
+#     intergenicHits[,c("anno", "type"):=.(paste0(unlist(lapply(intergenicHits$PRECEDEID, fetchLastElement)),"-",unlist(lapply(intergenicHits$FOLLOWID, fetchFirstElement))), "ingergenic")]
+#     intergenicHits <- cbind(snpInfo[intergenicHits$QUERYID,], intergenicHits[,.(anno,type)])
+#     # collapse annotation:
+#     intergenicHits <- intergenicHits[,.(anno=paste(anno,collapse = ",")),by=c("chrom", "pos", "pValue", "type")]
+#     rm(fetchLastElement, fetchFirstElement)
+#   }else{
+#     intergenicHits <- cpgHits[0,]
+#   }
+#   message("      Intergenic hits: ",nrow(promoterHits))
+#   rm(txinfo)
+#
+#   # combine all hits:
+#   snpHits <- do.call(rbind,list(cpgHits, enhancerHits, promoterHits, exonHits, cdsHits, intronHits, utr3Hits, utr5Hits, tfHits, splicingHits, intergenicHits))
+#   # For the rest of the failed to be annotated, all in intergenic:
+#   failedSnps <- fsetdiff(snpInfo, snpHits[,.(chrom, pos, pValue)])
+#   failedSnps$anno <- "-"
+#   failedSnps$type <- "ingergenic"
+#   snpHits <- rbind(snpHits, failedSnps)
+#   snpHits$logP <- log(snpHits$pValue, base=10)*(-1)
+#   snpHits$logP <- ifelse(snpHits$logP==0, 1e-6)
+#   snpHits$cutP <- cut(snpHits$logP,breaks= ceiling(seq(0,max(snpHits$log)+pValueBy, by=pValueBy)), include.lowest=TRUE, right=FALSE )
+#   snpHitsCount <- snpHits[,.(Num=nrow(.SD)), by= c("cutP", "type")]
+#
+#   # plot:
+#   ggplot(snpHitsCount)+
+#     geom_point(aes(x=cutP,y=Num,fill=type,shape=type),size=rel(3.8))+
+#     geom_line(aes(x=cutP,y=Num,color=type,group=type,linetype=type),size=rel(1.2))+
+#     scale_y_log10(breaks=10^c(0:floor(log(max(snpHitsCount$Num),10))), labels= as.character(as.integer(10^c(0:floor(log(max(snpHitsCount$Num),10))))) )+
+#     scale_shape_manual(values= c(0,1,2,5,6,21,22,23,24,25)[1:length(unique(snpHitsCount$type))] )+
+#     theme_classic()+
+#     xlab(expression(-log["10"]("Pvalue")))+
+#     ylab("Number of variants")+
+#     theme(
+#       axis.text.x = element_text(size = rel(1.3), angle=30, hjust=1, vjust=1),
+#       axis.text.y =element_text(size=rel(1.3)),
+#       axis.ticks.y = element_blank(),
+#       panel.grid.major.x = element_blank(), #设置面板网格
+#       panel.grid.major.y = element_line(size = 0.5),
+#       panel.grid.minor.x = element_blank(),
+#       axis.title=element_text(size=rel(1.3)),
+#       legend.title = element_text(face="bold",size=rel(1.1)),
+#       legend.text = element_text(size=rel(1.1)),
+#       legend.position = c(0.874,0.8),
+#       legend.margin = margin(0.1,0.1,0.1,0.1,"cm"),
+#       legend.background = element_rect(fill="white",
+#                                        size=0.5, linetype="solid",colour ="black")
+#     )
+#   snpHitsCount <- snpHits[,.(Num=nrow(.SD)), by= c("cutP", "type")]
+#   snpHitsCount[Num==1, "Num"] <- 2
+#   ggplot(snpHitsCount, aes(x = cutP ,y = Num,fill=type)) +
+#     geom_bar(stat = "identity",position =position_dodge(0.8), width = 0.7)+
+#     scale_y_log10( breaks=c(0.5,10^c(0:floor(log(max(snpHitsCount$Num),10)))), labels= as.character((c(0.5,10^c(0:floor(log(max(snpHitsCount$Num),10)))))) )+
+#     theme_bw()+
+#     theme(axis.text.x = element_text(size=rel(1.1)),
+#     axis.text.y = element_text(size=rel(1.1)),
+#     axis.title = element_text(size=rel(1.2)),
+#     legend.title = element_text(face="bold",size=rel(1.1)),
+#     legend.text = element_text(size=rel(1.1)),
+#     legend.position = c(0.874,0.8)
+#   )+
+#   xlab("ATC")+
+#   ylab("mean num of ADRs")	#坐标轴翻转
+#
+#
+#   snpHitsCount2 <- snpHits[]
+# }
+#
 
-
-
-
-
+#
+# a_680Ranges <- GenomicRanges::GRanges(a_680$seqnames,IRanges::IRanges(a_680$start, a_680$end), strand= "*")
+# a_680alls <- VariantAnnotation::locateVariants(a_680Ranges, txdb, VariantAnnotation::AllVariants(), ignore.strand=TRUE)
+# a_680intron <- locateVariants(a_680Ranges, txdb, IntronVariants()) %>% as.data.table
+# a_680out <- IRanges::findOverlaps(a_680Ranges, intronicParts(txdb) )
+#
+#
+# ####
+# intronParts <-  intronicParts(txdb)
+# exonParts <- exonicParts(txdb)
+# FiveUTRVariants
+# ThreeUTRVariants
+# PromoterVariants(upstream=1000, downstream=0)
+# SpliceSiteVariants
+#
+# tf <- fread("D:\\R_project\\Data\\Annotation\\tf_filtered_sorted_merge_sub.bed")
+# tfRanges <- GenomicRanges::GRanges(tf$V1,IRanges::IRanges(tf$V2, tf$V3), strand= "*")
+# geneHancer <- fread("D:\\R_project\\Data\\Annotation\\geneHancer.txt")
+# enhancerRanges <- GenomicRanges::GRanges(geneHancer$chrom, IRanges::IRanges(geneHancer$chromStart, geneHancer$chromEnd), strand= "*")
+# cpg <- fread("D:\\R_project\\Data\\Annotation\\cpgLsland.txt")
+# cpgRanges <- GenomicRanges::GRanges(cpg$chrom, IRanges::IRanges(cpg$chromStart, cpg$chromEnd), strand= "*")
+#
+# IRanges::findOverlaps(a_680Ranges, tfRanges )
+#
+# # enhancer > promoter > cds > exon > splice > 5UTR > 3UTR > intron > intergenics
+# # ignore.strand
+# ####
+#
+# IRanges::findOverlaps(snpRanges,tfRanges )
+# IRanges::findOverlaps(snpRanges,enhancerRanges )
+# IRanges::findOverlaps(snpRanges,cpgRanges )
+#
+# library(TxDb.Hsapiens.UCSC.hg38.knownGene )
+# txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+# alls <- VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::AllVariants())
+#
+# #
+# #
+# # ##
+# # 38:
+# load("D:/R_project/Data/GCST006085/envColocBoth_h38.RData")
+# snpRanges <- GenomicRanges::GRanges(ifelse(is.na(sentinelSnpDF$chr),"NA",sentinelSnpDF$chr),
+#                                     IRanges::IRanges(ifelse(is.na(sentinelSnpDF$position),-1,sentinelSnpDF$position), ifelse(is.na(sentinelSnpDF$position),-1,sentinelSnpDF$position)),
+#                                     strand= "*"
+#                                     # , geneInfo[,.(genes, geneSymbol, gencodeId, entrezGeneId, geneType, tss, description)]
+#                                     # ,seqinfo = GenomeInfoDb::Seqinfo(genome= stringr::str_split(unique(includedGenes$genomeBuild)[1],stringr::fixed("/"))[[1]][2])
+# )
+# library(TxDb.Hsapiens.UCSC.hg38.knownGene )
+# txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+# alls <- VariantAnnotation::locateVariants(snpRanges, txdb, VariantAnnotation::AllVariants())
+# tissueSiteDetail="Prostate"
+#
+# aa<- unique(aa[,.(seqnames, start,end,LOCATION)])
+#
+#
+# aaa <- merge( unique(aa[duplicated(aa[,.(seqnames,start,end)]),.(seqnames, start, end)]), aa, by=c("seqnames", "start", "end"))
+#
+#
+# enhancerRanges <- GenomicRanges::GRanges(ifelse(is.na(geneHancer$chrom),"NA",geneHancer$chrom),
+#                                          IRanges::IRanges(ifelse(is.na(geneHancer$chromStart),-1,geneHancer$chromStart), ifelse(is.na(geneHancer$chromEnd),-1,geneHancer$chromEnd)),
+#                                          strand= "*"
+# )
+#
+# tfRanges <- GenomicRanges::GRanges(tf$V1,IRanges::IRanges(tf$V2, tf$V3), strand= "*")
+#
+# IRanges::intersect(snpRanges,enhancerRanges)
