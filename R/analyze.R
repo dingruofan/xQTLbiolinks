bf72d19bad1c5f7f83b4e89c925b58a951949e1f
白衣卿相 <dingruofan1@foxmail.com>
2023-05-10 00:49:59 +0800

    Update analyze.R

--- R/analyze.R
+++ R/analyze.R
@@ -169,7 +169,7 @@ xQTLanalyze_getTraits <- function(sentinelSnpDF, detectRange=1e6, tissueSiteDeta
     stop("Because the genome version of eqtl associations only support GRCH38, sentinel SNP should be converted to GRCH38 before colocalization analysis if GRCH37 is provided.")
     genecodeVersion = "v19"
     datasetId="gtex_v7"
-  }if(genomeVersion =="grch37" & !grch37To38  & tissueSiteDetail==""){
+  }else if(genomeVersion =="grch37" & !grch37To38  & tissueSiteDetail==""){
     genecodeVersion = "v19"
     datasetId="gtex_v7"
   }else if( genomeVersion =="grch37" & grch37To38){
